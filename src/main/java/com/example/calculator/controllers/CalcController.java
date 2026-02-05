91f73d0cb482fcd72196a9df6c71973551cda03b
PaolaMMedina <162512964+PaolaMMedina@users.noreply.github.com>
2026-02-05 17:31:09 -0600

    Calculadora_multiplicacion

--- src/main/java/com/example/calculator/controllers/CalcController.java
+++ src/main/java/com/example/calculator/controllers/CalcController.java
@@ -13,9 +13,9 @@ public class CalcController{
         return "Holis mundo! Esta es la calculadora: ";
     }
     
-    @GetMapping("/resta/{n1}/{n2}")
-    public String resta(@PathVariable int n1, @PathVariable int n2){
-        int resultado=n1-n2; //va a agarrar el valor 1 que le mandemos desde el enlace para hacer la suma
+    @GetMapping("/mul/{n1}/{n2}")
+    public String mul(@PathVariable int n1, @PathVariable int n2){
+        int resultado=n1*n2; //va a agarrar el valor 1 que le mandemos desde el enlace para hacer la suma
         return "Resultado: "+resultado;
     }
 
