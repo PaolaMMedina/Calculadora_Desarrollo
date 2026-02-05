8c2cc6e0a91163a0de63e271c95aca1f1075617e
PaolaMMedina <162512964+PaolaMMedina@users.noreply.github.com>
2026-02-05 17:20:41 -0600

    SumaCalculadora
    Primer commit

---
+++ src/main/java/com/example/calculator/controllers/CalcController.java
@@ -0,0 +1,23 @@
+package com.example.calculator.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CalcController{
+
+    @GetMapping("/")
+    public String home(){
+        return "Holis mundo! Esta es la calculadora: ";
+    }
+    
+    @GetMapping("/suma/{n1}/{n2}")
+    public String suma(@PathVariable int n1, @PathVariable int n2){
+        int resultado=n1+n2; //va a agarrar el valor 1 que le mandemos desde el enlace para hacer la suma
+        return "Resultado: "+resultado;
+    }
+
+}
+
