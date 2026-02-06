aa6cf0eeb068ed71fc86857f640fce6f886dce92
PaolaMMedina <162512964+PaolaMMedina@users.noreply.github.com>
2026-02-05 19:50:00 -0600

    Ultima actualizacion RequestBody

--- src/main/java/com/example/calculator/controllers/CalcController.java
+++ src/main/java/com/example/calculator/controllers/CalcController.java
@@ -2,7 +2,13 @@ package com.example.calculator.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.calculator.model.Operation;
 
 
 @RestController
@@ -13,12 +19,11 @@ public class CalcController{
         return "Holis mundo! Esta es la calculadora: ";
     }
     
-    @GetMapping("/mul/{n1}/{n2}")
-    public String mul(@PathVariable int n1, @PathVariable int n2){
-        int resultado=n1*n2; //va a agarrar el valor 1 que le mandemos desde el enlace para hacer la suma
+    @GetMapping("/resta/{n1}/{n2}")
+    public String resta(@PathVariable int n1, @PathVariable int n2){
+        int resultado=n1-n2; //va a agarrar el valor 1 que le mandemos desde el enlace para hacer la resta
         return "Resultado: "+resultado;
     }
-
     //http://localhost:8080/suma-param?n1=5&n2=3
 
     @GetMapping("/suma-param")
@@ -28,6 +33,17 @@ public class CalcController{
     ) {
         return "Resultado: " + (n1 + n2);
     }
+
+    @RestController
+    @RequestMapping("/calc")
+    public class CalculatorController {
+
+        @PostMapping("/sum")
+        public double sum(@RequestBody Operation op) {
+            return op.getA() + op.getB();
+        }
+    }
+
 
 }
 
